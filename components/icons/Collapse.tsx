d9de26bee0e5ede2d647c9d3292720deee65fe60
Ives van Hoorne <ives.v.h@gmail.com>
2019-08-22 20:58:41 +0200

    Fix logs

--- components/icons/Collapse.tsx
+++ components/icons/Collapse.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const CollapseIcon: React.FC<{
   style?: React.CSSProperties;
 }> = props => (
-  <svg width={9} height={6} fill="none" {...props}>
+  <svg width={9} height={6} fill="none" style={props.style}>
     <path d="M4.5 6L0 0h9L4.5 6z" fill="#fff" />
   </svg>
 );
