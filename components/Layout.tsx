d5bccee4967e0ef497d7e06ddcb6b8bf91994adb
Sara <hey@iamsaravieira.com>
2019-08-27 20:18:19 -0500

    move icon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+import Link from "next/link";
+import { Back } from "./icons/Back";
 import { colors } from "../theme/colors";
 import { Header } from "./Header";
-import Link from "next/link";
 
 interface Props {
   title?: string;
@@ -81,22 +82,6 @@ const BreadCrumbs = styled.div`
   }
 `;
 
-const Icon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 6 11"
-    width="6px"
-    height="11px"
-  >
-    <path
-      fill="rgba(255, 255, 255, 0.6)"
-      fillOpacity="0.6"
-      d="M1.367 5.5L6 .707 5.316 0 0 5.5 5.316 11 6 10.25 1.367 5.5z"
-    />
-  </svg>
-);
-
 export const Layout: React.FC<Props> = ({
   username,
   title,
@@ -116,7 +101,7 @@ export const Layout: React.FC<Props> = ({
             as={`/status/${username}/${repo}`}
           >
             <a>
-              <Icon /> Back to Pull Requests
+              <Back /> Back to Pull Requests
             </a>
           </Link>
         ) : null}
@@ -126,7 +111,7 @@ export const Layout: React.FC<Props> = ({
             as={`/status/${username}/${repo}/pr/${selectedPr}`}
           >
             <a>
-              <Icon /> Back to PR #{selectedPr}
+              <Back /> Back to PR #{selectedPr}
             </a>
           </Link>
         ) : null}
---
+++ components/icons/Back.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Back = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 6 11"
+    width="6px"
+    height="11px"
+  >
+    <path
+      fill="rgba(255, 255, 255, 0.6)"
+      fillOpacity="0.6"
+      d="M1.367 5.5L6 .707 5.316 0 0 5.5 5.316 11 6 10.25 1.367 5.5z"
+    />
+  </svg>
+);
