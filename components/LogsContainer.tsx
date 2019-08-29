2739512f1bbbf07230c79685a00decc22829927a
Ives van Hoorne <ives.v.h@gmail.com>
2019-08-29 12:21:43 +0200

    Fix missing styled

--- components/LogsContainer.tsx
+++ components/LogsContainer.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import styled from "styled-components";
 import Ansi from "ansi-to-react";
 import { Status } from "../utils/api";
 import { colors } from "../theme/colors";
