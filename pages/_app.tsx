31e1d331983750cb6a71a237628e08b28eaf36e7
Ives van Hoorne <ives.v.h@gmail.com>
2019-09-06 14:54:21 +0200

    Optimize builds for next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,16 +12,6 @@ Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
 export default class CodeSandboxCI extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
   render() {
     const { Component, pageProps } = this.props;
 
