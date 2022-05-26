86119ae2f213348d2915946929707363912440ee
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2022-05-26 16:10:43 -0400

    Update reference to renamed GitHub App
    In preparation for renaming the existing GitHub App, this change fixes its URL.

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,4 @@
 export const LEARN_MORE_DOCUMENT_URL = 'https://codesandbox.io/docs/ci';
 export const LEARN_MORE_DOCUMENTATION_URL = 'https://u2edh.csb.app/docs.html';
 export const INSTALL_GITHUB_URL =
-  'https://github.com/apps/codesandbox/installations/new/';
+  'https://github.com/apps/codesandbox-ci/installations/new/';
